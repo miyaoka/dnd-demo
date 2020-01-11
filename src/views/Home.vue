4e36d0beeedd43a6dd804d533bb86a23fc6143b8
Masaya Kazama <miyan@t-p.jp>
2020-01-11 16:31:25 +0900

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <DomRenderer :dom="dom" />
+  </div>
+</template>
+
+<script lang="ts">
+import DomRenderer from '@/components/DomRenderer'
+import dom from '@/assets/dom.json'
+import tree from '@/assets/node.yaml'
+
+console.log(tree)
+export default {
+  name: 'home',
+  data() {
+    return {
+      dom
+    }
+  },
+  components: {
+    DomRenderer
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dom {
+  outline: 1px solid #999;
+}
+</style>
